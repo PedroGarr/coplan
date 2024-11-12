39b07ee04da378b1f69fd0e0dc3323bf001966cc
PedroGarr <pes.garr@gmail.com>
2024-11-12 12:28:20 +0100

    test: ignore this commit

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      Plan your next project and collaborate with your community
+    </div>
   )
 }
